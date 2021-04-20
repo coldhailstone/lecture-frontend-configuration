26625b7f9593141cd5d599c56117d8cef29b9fca
cold_hailstone <cksn1993@gmail.com>
2021-04-20 22:03:26 +0900

    바벨의 기본 개념

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src/app.js'
+        main: './app.js'
     },
     output: {
         path: path.resolve('./dist'),
@@ -27,6 +27,11 @@ module.exports = {
                     name: '[name].[ext]?[hash]',
                     limit: 20000
                 }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
             }
         ]
     },
